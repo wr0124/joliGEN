4cc09de61dfd7e40072045ccb7fc0ca11a7fb7c0
julie wang <julie.wang@jolibrain.com>
2026-01-07 16:44:28 +0100

    feat(ml): inference single image for b2b model

--- models/diffusion_networks.py
+++ models/diffusion_networks.py
@@ -98,16 +98,17 @@ def define_G(
 
     if model_type == "palette":
         in_channel = model_input_nc + model_output_nc
+    elif model_type == "b2b":
+        in_channel = model_input_nc
     else:  # CM
         if alg_diffusion_ddpm_cm_ft:
             in_channel = model_input_nc + model_output_nc
         else:
-            in_channel = model_input_nc
+            in_channel = model_input_nc + model_output_nc
         if (
             alg_diffusion_cond_embed != "" and alg_diffusion_cond_embed != "y_t"
         ) or alg_diffusion_task == "pix2pix":
             in_channel = model_input_nc + model_output_nc
-
     if "mask" in alg_diffusion_cond_embed:
         in_channel += alg_diffusion_cond_embed_dim
     if G_netG == "unet_mha":
